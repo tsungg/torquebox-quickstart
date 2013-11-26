1e24ad3ba27c4aab79b9e6953c8703e935bd8a9d
Tsu Ngg <tsungg@umbrellium.co.uk>
2013-11-26 11:38:14 +0000

    bundle install to data dir with local flag

--- .openshift/torquebox.sh
+++ .openshift/torquebox.sh
@@ -45,7 +45,7 @@ function torquebox_install() {
 function bundle_install() {
     if [ ! -d "${OPENSHIFT_REPO_DIR}/.bundle" ] && [ -f "${OPENSHIFT_REPO_DIR}/Gemfile" ]; then
         pushd ${OPENSHIFT_REPO_DIR} > /dev/null
-        jruby -J-Xmx256m -J-Dhttps.protocols=SSLv3 -S bundle install --without development test --deployment
+        jruby -J-Xmx256m -J-Dhttps.protocols=SSLv3 -S bundle install --without development test --deployment --local --path ${OPENSHIFT_DATA_DIR}/bundle
         popd > /dev/null
     fi
 }
